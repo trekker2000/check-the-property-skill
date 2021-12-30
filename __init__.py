161602c88216e0d1ba857fe57e9781647d4940e8
Jerry Bradshaw <trekker_777@live.com>
2021-12-30 00:37:34 +0000

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,15 @@
+from mycroft import MycroftSkill, intent_file_handler
+
+
+class CheckTheProperty(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+
+    @intent_file_handler('property.the.check.intent')
+    def handle_property_the_check(self, message):
+        self.speak_dialog('property.the.check')
+
+
+def create_skill():
+    return CheckTheProperty()
+
